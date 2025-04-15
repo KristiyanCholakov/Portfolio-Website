f91a2106d625890fd232df66911d36866a584552
Cholakov Krisityan Kamenov <50497496+KrisCholakov02@users.noreply.github.com>
2025-04-15 11:01:20 +0300

    add content visibility control to About component for improved user experience + fixing bugs with some animations flashing

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  semi: false,
+  printWidth: 100,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.ts',
+}
